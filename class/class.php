af124714d2c8bca6dd6c85fa98b91af9a7e5a992
daniel torres <daniel.torrer@gmail.com>
2012-11-07 20:31:54 -0600

    galeria

--- class/class.php
+++ class/class.php
@@ -4,11 +4,11 @@ class Conectar {
 
 	public static function con() {
 
-		//$conexion = mysql_connect("mysql51-014.wc1.ord1.stabletransit.com", "709717_daniel", "cabasgym") or
-		$conexion = mysql_connect("localhost", "root", "") or
+		$conexion = mysql_connect("mysql51-014.wc1.ord1.stabletransit.com", "709717_daniel", "cabasgym") or
+		//$conexion = mysql_connect("localhost", "root", "") or
 				die("Error de conexion: " . mysql_error());  // hacemos conexion al serv
-		//mysql_select_db("709717_mosaico") or
-		mysql_select_db("mosaico") or
+		mysql_select_db("709717_mosaico") or
+		//mysql_select_db("mosaico") or
 				die("Error de conexion: " . mysql_error()); //
 		mysql_query("SET CHARACTER SET utf-8");
 
@@ -36,7 +36,7 @@ class queries {
 	}
 
 	public function insertData($nombre, $sexo, $edad, $mail, $cp, $estado, $hoy, $futuro, $prioridad, $accion) {
-		$sql = "Insert into registros values (null ,'" . $nombre . "','" . $sexo . "','" . $edad . "','" . $mail . "','" . $cp . "','" . $estado . "',null,null,null,null,null,null,null,'" . $hoy . "','" . $futuro . "','" . $prioridad . "','" . $accion . "')";
+		$sql = "Insert into registros values (null ,'" . $nombre . "','" . $sexo . "','" . $edad . "','" . $mail . "','" . $cp . "','" . $estado . "',null,null,null,null,null,null,'si','" . $hoy . "','" . $futuro . "','" . $prioridad . "','" . $accion . "')";
 		echo $sql;
 		$result = array();
 		
