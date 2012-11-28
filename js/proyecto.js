4e052ac4d887d00d8fd5c342dff5fbced1513a08
daniel torres <daniel.torrer@gmail.com>
2012-11-27 20:44:45 -0600

    imagen variable
    lhjasd

--- js/proyecto.js
+++ js/proyecto.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-    $("#title").click(function(){
-       // $("#modaldesc .modal-header h3").html("Mosaico Tijuana")
-        //$("#modaldesc .modal-body").html("<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>")
-		
+	var num1 = Math.floor(Math.random()*1000)%24+1
+	var num2 = Math.floor(Math.random()*1000)%24+1
+	$("#btn-izq").css("background", 'url(img/full'+num1+'.jpg) no-repeat center center fixed')
+	$("#btn-der").css("background", 'url(img/full'+num2+'.jpg) no-repeat center center fixed')
+	
+	$("#title").click(function(){
+
 		$("#modaldesc").modal().css({
             'width': function() {
                 return ($(document).width() *	.8) + 'px';  
